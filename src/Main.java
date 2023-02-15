195f46d923d61471b48646196418b9a93f54dd8f
Tichinskiy Vladislav <whiteknight19283@gmail.com>
2023-02-15 22:56:15 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = -5 + 8 * 6;
+        int b = (55 + 9) % 9;
+        int c = 20 + -3 * 5 / 8;
+        int d = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println(a + System.lineSeparator() + b + System.lineSeparator() + c + System.lineSeparator() + d);
+
+    }
+}
